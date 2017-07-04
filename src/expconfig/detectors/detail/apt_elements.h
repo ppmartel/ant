b92426b412ed5cb0b9872929548d6f34f2dac8e4
Hannah Seymour <seymourhannah4@gmail.com>
2017-07-04 11:20:46 +0200

    APT element file added to detectors/detail

--- src/expconfig/detectors/detail/apt_elements.h
+++ src/expconfig/detectors/detail/apt_elements.h
@@ -1,4 +1,22 @@
-#ifndef APT_ELEMENTS_H
-#define APT_ELEMENTS_H
-
-#endif // APT_ELEMENTS_H
+#include <detectors/APT.h>
+const std::vector<ant::expconfig::detector::APT::Element_t>
+ant::expconfig::detector::APT_2017::elements_init =
+{
+    //channel, ADC #, TDC #
+    { 1, 216, 232},
+    { 2, 215, 233},
+    { 3, 218, 234},
+    { 4, 219, 235},
+    { 5, 220, 236},
+    { 6, 221, 237},
+    { 7, 222, 238},
+    { 8, 223, 239},
+    { 9, 224, 240},
+    {10, 225, 241},
+    {11, 226, 242},
+    {12, 227, 243},
+    {13, 228, 244},
+    {14, 229, 245},
+    {15, 214, 246},
+    {16, 231, 247},
+};
