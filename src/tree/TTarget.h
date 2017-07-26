0492a9aea9d424b35e65f9a0afe54d7cbb35a89b
Hannah Seymour <seymourhannah4@gmail.com>
2017-07-26 15:59:43 +0200

    Tree: TTarget: Added struct ActivePhotons{} to read number of photons from cluster hit

--- src/tree/TTarget.h
+++ src/tree/TTarget.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "tree/TDetectorReadHit.h"
+
 #include "base/types.h"
 #include "base/std_ext/math.h"
 
@@ -12,7 +14,49 @@ namespace ant {
 
 struct TTarget {
 
-    //int ActivePhotons;
+     struct ActivePhotons
+     {
+
+         struct Datum
+         {
+            unsigned Channel;
+            TDetectorReadHit::Value_t Value;
+
+            Datum(unsigned channel, const TDetectorReadHit::Value_t& value):
+                Channel(channel),
+                Value(value)
+            {}
+
+            template<class Archive>
+            void serialize(Archive& archive) {
+                archive(Channel, Value);
+            }
+            Datum() {}
+         };
+
+        std::list<Datum> Data;
+        unsigned Channel;
+        double PhotonNumber;
+
+        ActivePhotons(unsigned channel, double photonnumber):
+            Channel(channel), PhotonNumber(photonnumber)
+        {}
+
+        template<class Archive>
+        void serialize(Archive& archive) {
+            archive(Channel, PhotonNumber);
+        }
+
+        friend std::ostream& operator<<( std::ostream& s, const ActivePhotons& o) {
+            s << "Channel =" << o.Channel << ", Number Photons=" << o.PhotonNumber;
+            for(const auto& datum : o.Data) {
+                s << "Channel=" << datum.Channel << "=" << datum.Value;
+            }
+            return s;
+        }
+        ActivePhotons()=default;
+        };
+
     vec3 Vertex;
 
     TTarget(const vec3& vertex = {std_ext::NaN, std_ext::NaN, std_ext::NaN}):
